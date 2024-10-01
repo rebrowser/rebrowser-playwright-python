aebb246f48650b81f84a789a9c458ce98251f5eb
nwebson <nwebson@outlook.com>
2024-10-01 15:05:06 -0700

    patched

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ import glob
 import os
 import platform
 import shutil
+import tempfile
+import urllib.request
+import tarfile
+import re
 import subprocess
 import sys
 import zipfile
@@ -30,8 +34,59 @@ except ImportError:
     InWheel = None
 from wheel.bdist_wheel import bdist_wheel as BDistWheelCommand
 
-driver_version = "1.47.0-beta-1726138322000"
+driver_version = "1.47.2"
 
+def prepare_rebrowser_sources():
+    old_path = "playwright"
+    new_path = "rebrowser_playwright"
+    shutil.rmtree(new_path, ignore_errors=True)
+    shutil.copytree(old_path, new_path)
+    
+    print(f"[rebrowser-patches] preparing new sources in {new_path}")
+    for root, dirs, files in os.walk(new_path):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                new_content = content
+                
+                new_content = re.sub(r'= playwright\._impl', '= rebrowser_playwright._impl', new_content)
+                new_content = re.sub(r'from playwright.', 'from rebrowser_playwright.', new_content)
+                new_content = re.sub(r'import playwright.', 'import rebrowser_playwright.', new_content)
+                new_content = re.sub(r'import playwright', 'import rebrowser_playwright as playwright', new_content)
+                
+                if new_content != content:
+                    with open(file_path, 'w') as f:
+                        f.write(new_content)
+                    print(f"[rebrowser-patches] updated imports in {file_path}")
+
+def replace_driver_with_rebrowser(path: str):
+    package_folder = path + "/package"
+    package_name = "rebrowser-playwright-core"
+    package_version = "1.47.100"
+    
+    print(f"[rebrowser-patches] package_version = {package_version}, path = {path}")
+    
+    # clean old package folder
+    if os.path.exists(package_folder):
+        shutil.rmtree(package_folder)
+    os.makedirs(package_folder)
+
+    # Create a temporary directory for downloading
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Construct the NPM package URL
+        npm_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-{package_version}.tgz"
+
+        # Download the package
+        tgz_file = os.path.join(temp_dir, f"{package_name}.tgz")
+        urllib.request.urlretrieve(npm_url, tgz_file)
+
+        # Extract the package
+        with tarfile.open(tgz_file, "r:gz") as tar:
+            tar.extractall(path=path)
+
+    print(f"[rebrowser-patches] {package_name} has been downloaded and extracted to {path}")
 
 def extractall(zip: zipfile.ZipFile, path: str) -> None:
     for name in zip.namelist():
@@ -70,12 +125,13 @@ class PlaywrightBDistWheelCommand(BDistWheelCommand):
         self.all = False
 
     def run(self) -> None:
+        prepare_rebrowser_sources()
         shutil.rmtree("build", ignore_errors=True)
         shutil.rmtree("dist", ignore_errors=True)
         shutil.rmtree("playwright.egg-info", ignore_errors=True)
         super().run()
         os.makedirs("driver", exist_ok=True)
-        os.makedirs("playwright/driver", exist_ok=True)
+        os.makedirs("rebrowser_playwright/driver", exist_ok=True)
         base_wheel_bundles: List[Dict[str, str]] = [
             {
                 "wheel": "macosx_10_13_x86_64.whl",
@@ -147,6 +203,7 @@ class PlaywrightBDistWheelCommand(BDistWheelCommand):
             )
             with zipfile.ZipFile(zip_file, "r") as zip:
                 extractall(zip, f"driver/{wheel_bundle['zip_name']}")
+                replace_driver_with_rebrowser(f"driver/{wheel_bundle['zip_name']}")
             wheel_location = without_platform + wheel_bundle["wheel"]
             shutil.copy(base_wheel_location, wheel_location)
             with zipfile.ZipFile(wheel_location, "a") as zip:
@@ -155,9 +212,9 @@ class PlaywrightBDistWheelCommand(BDistWheelCommand):
                     for file in files:
                         from_path = os.path.join(dir_path, file)
                         to_path = os.path.relpath(from_path, driver_root)
-                        zip.write(from_path, f"playwright/driver/{to_path}")
+                        zip.write(from_path, f"rebrowser_playwright/driver/{to_path}")
                 zip.writestr(
-                    "playwright/driver/README.md",
+                    "rebrowser_playwright/driver/README.md",
                     f"{wheel_bundle['wheel']} driver package",
                 )
         os.remove(base_wheel_location)
@@ -194,27 +251,28 @@ class PlaywrightBDistWheelCommand(BDistWheelCommand):
         download_driver(zip_name)
         zip_file = f"driver/playwright-{driver_version}-{zip_name}.zip"
         with zipfile.ZipFile(zip_file, "r") as zip:
-            extractall(zip, "playwright/driver")
+            extractall(zip, "rebrowser_playwright/driver")
 
 
 setup(
-    name="playwright",
+    name="rebrowser_playwright",
+    version="1.47.0",
     author="Microsoft Corporation",
     author_email="",
     description="A high-level API to automate web browsers",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     license="Apache-2.0",
-    url="https://github.com/Microsoft/playwright-python",
+    url="https://github.com/rebrowser/rebrowser-playwright-python",
     project_urls={
-        "Release notes": "https://github.com/microsoft/playwright-python/releases",
+        "Release notes": "https://github.com/rebrowser/rebrowser-playwright-python/releases",
     },
     packages=[
-        "playwright",
-        "playwright.async_api",
-        "playwright.sync_api",
-        "playwright._impl",
-        "playwright._impl.__pyinstaller",
+        "rebrowser_playwright",
+        "rebrowser_playwright.async_api",
+        "rebrowser_playwright.sync_api",
+        "rebrowser_playwright._impl",
+        "rebrowser_playwright._impl.__pyinstaller",
     ],
     include_package_data=True,
     install_requires=[
@@ -240,8 +298,8 @@ setup(
     cmdclass={"bdist_wheel": PlaywrightBDistWheelCommand},
     entry_points={
         "console_scripts": [
-            "playwright=playwright.__main__:main",
+            "rebrowser_playwright=rebrowser_playwright.__main__:main",
         ],
-        "pyinstaller40": ["hook-dirs=playwright._impl.__pyinstaller:get_hook_dirs"],
+        "pyinstaller40": ["hook-dirs=rebrowser_playwright._impl.__pyinstaller:get_hook_dirs"],
     },
 )
